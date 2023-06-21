930bc04423fb0e2e7a92c7083f8364a60bb1831e
AAL <adrienne.lutz@outlook.com>
2023-06-21 17:05:50 -0400

    Clean-up/Miscellaneous updates

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // My Quote Generator
 
-
-//-------------------------
-//Function: quoteFactory
-//-------------------------
+//--------------------------------
+//Initalize quoteFactory function
+//--------------------------------
 const quoteFactory = (category, key, quoteNum, quote, sourceInfo) => {
     return {
         _category: category,
@@ -46,9 +45,6 @@ function getRandomItem(array) {
     return array[index];
 }
 
-
-
-
 //-------------------------
 //Function Build Data
 //-------------------------
@@ -83,11 +79,8 @@ function buildData() {
     const film4Quotes = buildf4Quotes(category);
     const filmQuotes = [film1Quotes, film2Quotes, film3Quotes, film4Quotes];
 
-    //return arrary of quotes for each category
+    //return array of quotes for each category
     return [peopleQuotes, literaryWorksQuotes, filmQuotes];
-
-
-
 }
 
 //------------------------------
@@ -96,24 +89,24 @@ function buildData() {
 function buildEinsteinQuotes(category) {
 
     let keyID = "Albert Einstein";
-    const einsteinQ1 = quoteFactory(category, keyID, 1,
+    const quote1 = quoteFactory(category, keyID, 1,
         "Blind obedience to authority is the greatest enemy of truth.",
         "Letter to Jost Winteler (July 8th, 1901), quoted in The Private Lives of Albert Einstein by Roger Highfields and Paul Carter (1993), p. 79. Einstein had been annoyed that Paul Drude, editor of Annalen der Physik, had dismissed some criticisms Einstein made of Drude's electron theory of metals.");
-    const einsteinQ2 = quoteFactory(category, keyID, 2,
+    const quote2 = quoteFactory(category, keyID, 2,
         "In living through this 'great epoch,' it is difficult to reconcile oneself to the fact that one belongs to that mad, degenerate species that boasts of its free will. How I wish that somewhere there existed an island for those who are wise and of good will! In such a place even I should be an ardent patriot!",
         "Letter to Paul Ehrenfest, early December 1914. Collected Papers of Albert Einstein, Vol. 8, Doc. 39. Quoted in The New Quotable Einstein by Alice Calaprice (2005), p. 3");
-    const einsteinQ3 = quoteFactory(category, keyID, 3,
+    const quote3 = quoteFactory(category, keyID, 3,
         "In so far as theories of mathematics speak about reality, they are not certain, and in so far as they are certain, they do not speak about reality.",
         "Geometrie and Erfahrung (1921) pp. 3-4 link.springer.com as cited by Karl Popper, The Two Fundamental Problems of the Theory of Knowledge (2014) Tr. Andreas Pickel, Ed. Troels Eggers Hansen.");
-    const einsteinQ4 = quoteFactory(category, keyID, 4,
-        "Test Einstein Quote 4",
-        "");
-    const einsteinQ5 = quoteFactory(category, keyID, 5,
-        "Test Einstein Quote 5",
-        "");
+    const quote4 = quoteFactory(category, keyID, 4,
+        "Subtle is the Lord, but malicious He is not.",
+        "Remark made during Einstein's first visit to Princeton University (April 1921) as quoted in Einstein (1973) by R. W. Clark, Ch. 14. 'God is slick, but he ain't mean' is a variant translation of this (1946) Unsourced variant: 'God is subtle but he is not malicious.'");
+    const quote5 = quoteFactory(category, keyID, 5,
+        "[I do not] carry such information in my mind since it is readily available in books. ...The value of a college education is not the learning of many facts but the training of the mind to think.",
+        "In response to not knowing the speed of sound as included in the Edison Test: New York Times (18 May 1921); Einstein: His Life and Times (1947) Philipp Frank, p. 185; Einstein, A Life (1996) by Denis Brian, p. 129; 'Einstein Due Today' (February 2005) edited by József Illy, Manuscript 25-32 of the Einstein Paper Project; all previous sources as per Einstein His Life and Universe (2007) by Walter Isaacson, p. 299");
 
     // return array of all Einstein quotes
-    return [einsteinQ1, einsteinQ2, einsteinQ3, einsteinQ4, einsteinQ5];
+    return [quote1, quote2, quote3, quote4, quote5];
 }
 
 //------------------------------
@@ -122,47 +115,51 @@ function buildEinsteinQuotes(category) {
 function buildAristotleQuotes(category) {
 
     keyID = "Aristotle";
-    const aristotleQ1 = quoteFactory(category, keyID, 1,
+    const quote1 = quoteFactory(category, keyID, 1,
         "Knowledge of the fact differs from knowledge of the reason for the fact",
         "Posterior Analytics I. 13, 78a.22");
-    const aristotleQ2 = quoteFactory(category, keyID, 2,
+    const quote2 = quoteFactory(category, keyID, 2,
         "[I]t is impossible for motion to subsist without place, and void, and time.",
         "Physics Book III, Ch. I, p. 136.");
-    const aristotleQ3 = quoteFactory(category, keyID, 3,
-        "Test Aristotle Quote 3",
-        "");
-    const aristotleQ4 = quoteFactory(category, keyID, 4,
-        "Test Aristotle Quote 4",
-        "");
-    const aristotleQ5 = quoteFactory(category, keyID, 5,
-        "Test Aristotle Quote 5",
-        "");
+    const quote3 = quoteFactory(category, keyID, 3,
+        "We should venture on the study of every kind of animal without distaste; for each and all will reveal to us something natural and something beautiful",
+        "'Parts of Animals' Book I, 645a.21");
+    const quote4 = quoteFactory(category, keyID, 4,
+        "Music directly represents the passion of the soul. If one listens to the wrong kind of music, he will become the wrong kind of person.est Aristotle Quote 4",
+        "Aristotle, Complete works of Aristotle, Vol. I");
+    const quote5 = quoteFactory(category, keyID, 5,
+        "It is the mark of an educated man to look for precision in each class of things just so far as the nature of the subject admits; it is evidently equally foolish to accept probable reasoning from a mathematician and to demand from a rhetorician scientific proofs.",
+        "'Nicomachean Ethics' Book I, 1094b.24");
+    const quote6 = quoteFactory(category, keyID, 6,
+        "Piety requires us to honor truth above our friends.",
+        "'Nicomachean Ethics' Book I, 1096a.16");
+
 
     // return array of all Aritotle quotes
-    return [aristotleQ1, aristotleQ2, aristotleQ3, aristotleQ4, aristotleQ5];
+    return [quote1, quote2, quote3, quote4, quote5, quote6];
 
 }
 
 function buildLaoziQuotes(category) {
 
     keyID = "Laozi";
-    const laoziQ1 = quoteFactory(category, keyID, 1,
+    const quote1 = quoteFactory(category, keyID, 1,
         " Test Laozi quote 1",
         "");
-    const laoziQ2 = quoteFactory(category, keyID, 2,
+    const quote2 = quoteFactory(category, keyID, 2,
         " Test Laozi quote 2",
         "");
-    const laoziQ3 = quoteFactory(category, keyID, 3,
+    const quote3 = quoteFactory(category, keyID, 3,
         " Test Laozi quote 3",
         "");
-    const laoziQ4 = quoteFactory(category, keyID, 4,
+    const quote4 = quoteFactory(category, keyID, 4,
         " Test Laozi quote 4",
         "");
-    const laoziQ5 = quoteFactory(category, keyID, 5,
+    const quote5 = quoteFactory(category, keyID, 5,
         " Test Laozi quote 5",
         "");
 
-    return [laoziQ1, laoziQ2, laoziQ3, laoziQ4, laoziQ5];
+    return [quote1, quote2, quote3, quote4, quote5];
 
 }
 
