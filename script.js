fa02f65e3af1b578f31764d0d78d61ecff9874b7
AAL <adrienne.lutz@outlook.com>
2023-06-21 11:45:55 -0400

    Continue refactoring to make more modular

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-// My Generator
+// My Quote Generator
 
-// Build Data
-// -- Data consists of a nested set of arrays  
+
+//-------------------------
+//Function: quoteFactory
+//-------------------------
+const quoteFactory = (category, key, quoteNum, quote, sourceInfo) => {
+    return {
+        _category: category,
+        _keyID: key,
+        _quoteNum: quoteNum,
+        _quote: quote,
+        _source_info: sourceInfo,
+
+        //TO DO: Add setters and getters
+    }
+}
+
+// Build Data -- Data consists of a nested set of arrays  
 const allQuotesbyCateogry = buildData();
 
-//get random cateogry array -- eg. People, Literary Works, Films
+//get random cateogry array -- i.e. People Quotes, Literary Works Quotes or Film Quotes etc
 const selectedCategoryArray = getRandomItem(allQuotesbyCateogry);
 
-//get random quote array in selected Category -- e.g. Aristotle quotes, Laozi quotes, The Little Prince quotes depending on category  
-const selectedQuoteArray = getRandomItem(selectedCategoryArray);
+//get random subject array in the selected Category i.e. if the selected category is
+//people then randomly select array of Aristotle quotes, Laozi quotes or Einstein quotes etc.   
+const selectedSubjectArray = getRandomItem(selectedCategoryArray);
 
-//get random quote object in selected Quote Array within selected Category 
-const selectedQuote = getRandomItem(selectedQuoteArray);
+//get random quote object in selected subject array i.e. if selected subject is "Aristotle", randomly pick 
+// one of the quotes in array of Aristotle quotes.
+const selectedQuote = getRandomItem(selectedSubjectArray);
 
 //Display quote to terminal
 console.log(selectedQuote._quote);
@@ -20,37 +37,65 @@ if (selectedQuote._source_info)
 else
     console.log("-- " + selectedQuote._keyID);
 
-    
+
+//-------------------------
 //Function: getRandomItem
+//-------------------------
 function getRandomItem(array) {
     const index = Math.floor(Math.random() * (array.length - 1));
     return array[index];
 }
+
+
+
+
+//-------------------------
 //Function Build Data
+//-------------------------
 function buildData() {
 
-    const quoteFactory = (category, key, quoteNum, quote, sourceInfo) => {
-        return {
-            _category: category,
-            _keyID: key,
-            _quoteNum: quoteNum,
-            _quote: quote,
-            _source_info: sourceInfo,
-
-            //TO DO: Add setters and getters
-        }
-    }
-
-    let category = "";
-    let keyID = "";
+    //-------------------------------------
+    //Build "People" quote arrays
+    //-------------------------------------
+    let category = "People";
+    const einsteingQuotes = buildEinsteinQuotes(category);
+    const aristotleQuotes = buildAristotleQuotes(category);
+    const laoziQuotes = buildLaoziQuotes(category);
+    const peopleQuotes = [einsteingQuotes, aristotleQuotes, laoziQuotes];
 
     //-------------------------------------
-    //Build "People" quote objects
+    //Build "Literary Works" quote arrays
     //-------------------------------------
-    category = "People";
+    category = "Literary Works";
+    const duneQuotes = buildDuneQuotes(category);
+    const theLittlePrinceQuotes = buildLittlePrinceQuotes(category);
+    const lw3Quotes = buildlw3Quotes(category);
+    const lw4Quotes = buildlw4Quotes(category);
+    const literaryWorksQuotes = [duneQuotes, theLittlePrinceQuotes, lw3Quotes, lw4Quotes];
 
-    //-- Person #1 - Einstein Quotes
-    keyID = "Albert Einstein";
+    //-------------------------------------
+    //Build "Film" quote arrays
+    //-------------------------------------
+    category = "Films";
+    const film1Quotes = buildf1Quotes(category);
+    const film2Quotes = buildf2Quotes(category);
+    const film3Quotes = buildf3Quotes(category);
+    const film4Quotes = buildf4Quotes(category);
+    const filmQuotes = [film1Quotes, film2Quotes, film3Quotes, film4Quotes];
+
+    //return arrary of quotes for each category
+    return [peopleQuotes, literaryWorksQuotes, filmQuotes];
+
+
+
+}
+
+//------------------------------
+//Function: buildEinsteinQuotes
+//------------------------------
+function buildEinsteinQuotes(category) {
+
+    let keyID = "Albert Einstein";
     const einsteinQ1 = quoteFactory(category, keyID, 1,
         "Blind obedience to authority is the greatest enemy of truth.",
         "Letter to Jost Winteler (July 8th, 1901), quoted in The Private Lives of Albert Einstein by Roger Highfields and Paul Carter (1993), p. 79. Einstein had been annoyed that Paul Drude, editor of Annalen der Physik, had dismissed some criticisms Einstein made of Drude's electron theory of metals.");
@@ -66,9 +111,16 @@ function buildData() {
     const einsteinQ5 = quoteFactory(category, keyID, 5,
         "Test Einstein Quote 5",
         "");
-    const einsteingQuotes = [einsteinQ1, einsteinQ2, einsteinQ3, einsteinQ4, einsteinQ5];
 
-    //-- Person #2 - Aristotle
+    // return array of all Einstein quotes
+    return [einsteinQ1, einsteinQ2, einsteinQ3, einsteinQ4, einsteinQ5];
+}
+
+//------------------------------
+//Function: buildAristotleQuotes
+//------------------------------
+function buildAristotleQuotes(category) {
+
     keyID = "Aristotle";
     const aristotleQ1 = quoteFactory(category, keyID, 1,
         "Knowledge of the fact differs from knowledge of the reason for the fact",
@@ -85,9 +137,14 @@ function buildData() {
     const aristotleQ5 = quoteFactory(category, keyID, 5,
         "Test Aristotle Quote 5",
         "");
-    const aristotleQuotes = [aristotleQ1, aristotleQ2, aristotleQ3, aristotleQ4, aristotleQ5];
 
-    //-- Person #3 - Laozi
+    // return array of all Aritotle quotes
+    return [aristotleQ1, aristotleQ2, aristotleQ3, aristotleQ4, aristotleQ5];
+
+}
+
+function buildLaoziQuotes(category) {
+
     keyID = "Laozi";
     const laoziQ1 = quoteFactory(category, keyID, 1,
         " Test Laozi quote 1",
@@ -104,15 +161,13 @@ function buildData() {
     const laoziQ5 = quoteFactory(category, keyID, 5,
         " Test Laozi quote 5",
         "");
-    const laoziQuotes = [laoziQ1, laoziQ2, laoziQ3, laoziQ4, laoziQ5];
 
+    return [laoziQ1, laoziQ2, laoziQ3, laoziQ4, laoziQ5];
 
-    //-------------------------------------
-    //Build "Literary Works" quote objects
-    //-------------------------------------
-    category = "Literary Works";
+}
 
-    //-- Literary Work #1 - Dune
+function buildDuneQuotes(category) {
+
     keyID = "Dune";
     const duneQ1 = quoteFactory(category, keyID, 1,
         "A popular man arouses the jealousy of the powerful.",
@@ -129,19 +184,21 @@ function buildData() {
     const duneQ5 = quoteFactory(category, keyID, 5,
         " Test dune quote 5",
         "");
-    const duneQuotes = [duneQ1, duneQ2, duneQ3, duneQ4, duneQ5];
 
+    return [duneQ1, duneQ2, duneQ3, duneQ4, duneQ5];
+}
 
-    //-- Literary Work #2 - The Little Prince
+function buildLittlePrinceQuotes(category) {
+
     keyID = "Le Petit Prince (The Little Prince)";
     const theLittlePrinceQ1 = quoteFactory(category, keyID, 1,
-        "Les grandes personnes ne comprennent jamais rien toutes seules, et c'est fatigant, pour les enfants, de toujours et toujours leur donner des explications. (Grown-ups never understand anything by themselves, and it is tiresome for children to be always and forever explaining things to them.",
+        "Grown-ups never understand anything by themselves, and it is tiresome for children to be always and forever explaining things to them.",
         "Chapter I");
     const theLittlePrinceQ2 = quoteFactory(category, keyID, 2,
-        "Quand on veut un mouton, c'est la preuve qu'on existe. (If somebody wants a sheep, that is proof that somebody exists.)",
+        "If somebody wants a sheep, that is proof that somebody exists.",
         "Chapter IV");
     const theLittlePrinceQ3 = quoteFactory(category, keyID, 3,
-        "C'est véritablement utile puisque c'est joli. (It is truly useful since it is beautiful.)",
+        "It is truly useful since it is beautiful.",
         "Chapter XIV");
     const theLittlePrinceQ4 = quoteFactory(category, keyID, 4,
         " Test The Little Prince quote 4",
@@ -149,10 +206,13 @@ function buildData() {
     const theLittlePrinceQ5 = quoteFactory(category, keyID, 5,
         " Test The Little Prince quote 5",
         "");
-    const theLittlePrinceQuotes = [theLittlePrinceQ1, theLittlePrinceQ2, theLittlePrinceQ3,
-        theLittlePrinceQ4, theLittlePrinceQ5];
 
-    //-- Literary Work #3 - TEST LW 3
+    return [theLittlePrinceQ1, theLittlePrinceQ2, theLittlePrinceQ3,
+        theLittlePrinceQ4, theLittlePrinceQ5];
+}
+
+function buildlw3Quotes(category) {
+
     keyID = "Literary Work #3";
     const LW3Q1 = quoteFactory(category, keyID, 1,
         "Test LW3 - Quote 1",
@@ -169,9 +229,13 @@ function buildData() {
     const LW3Q5 = quoteFactory(category, keyID, 5,
         "Test LW3 - Quote 5",
         "");
-    const LW3Quotes = [LW3Q1, LW3Q2, LW3Q3, LW3Q4, LW3Q5];
 
-    //-- Literary Work #4 - TEST LW 4
+    return [LW3Q1, LW3Q2, LW3Q3, LW3Q4, LW3Q5];
+
+}
+
+function buildlw4Quotes(category) {
+
     keyID = "Literary Work #4";
     const LW4Q1 = quoteFactory(category, keyID, 1,
         "Test LW4 - Quote 1",
@@ -188,14 +252,12 @@ function buildData() {
     const LW4Q5 = quoteFactory(category, keyID, 5,
         "Test LW4 - Quote 5",
         "");
-    const LW4Quotes = [LW4Q1, LW4Q2, LW4Q3, LW4Q4, LW4Q5];
 
-    //-------------------------------------
-    //Build "Film" quote objects
-    //-------------------------------------
-    category = "Films";
+    return [LW4Q1, LW4Q2, LW4Q3, LW4Q4, LW4Q5];
+}
 
-    //-- Film #1 - TEST Film 1
+
+function buildf1Quotes(category) {
     keyID = "Film #1";
     const film1Q1 = quoteFactory(category, keyID, 1,
         "Test Film #1 - Quote 1",
@@ -212,9 +274,13 @@ function buildData() {
     const film1Q5 = quoteFactory(category, keyID, 5,
         "Test Film #1 - Quote 5",
         "");
-    const film1Quotes = [film1Q1, film1Q2, film1Q3, film1Q4, film1Q5];
 
-    //-- Film #2 - TEST Film 2
+    return [film1Q1, film1Q2, film1Q3, film1Q4, film1Q5];
+}
+
+
+function buildf2Quotes(category) {
+
     keyID = "Film #2";
     const film2Q1 = quoteFactory(category, keyID, 1,
         "Test Film #2 - Quote 1",
@@ -231,9 +297,12 @@ function buildData() {
     const film2Q5 = quoteFactory(category, keyID, 5,
         "Test Film #2 - Quote 5",
         "");
-    const film2Quotes = [film2Q1, film2Q2, film2Q3, film2Q4, film2Q5];
 
-    //-- Film #3 - TEST Film 3
+    return [film2Q1, film2Q2, film2Q3, film2Q4, film2Q5];
+}
+
+function buildf3Quotes(category) {
+
     keyID = "Film #3";
     const film3Q1 = quoteFactory(category, keyID, 1,
         "Test Film #3 - Quote 1",
@@ -250,15 +319,28 @@ function buildData() {
     const film3Q5 = quoteFactory(category, keyID, 5,
         "Test Film #3 - Quote 5",
         "");
-    const film3Quotes = [film3Q1, film3Q2, film3Q3, film3Q4, film3Q5];
+    return [film3Q1, film3Q2, film3Q3, film3Q4, film3Q5];
+}
 
-    // Create array of quotes for each category
-    const peopleQuotes = [einsteingQuotes, aristotleQuotes, laoziQuotes];
-    const literaryWorksQuotes = [duneQuotes, theLittlePrinceQuotes, LW3Quotes, LW4Quotes];
-    const filmQuotes = [film1Quotes, film2Quotes, film3Quotes];
+function buildf4Quotes(category) {
 
-    // Return array of all quotes by Category
-    return [peopleQuotes, literaryWorksQuotes, filmQuotes];
+    keyID = "Film #4";
+    const quote1 = quoteFactory(category, keyID, 1,
+        "Test Film #4 - Quote 1",
+        "");
+    const quote2 = quoteFactory(category, keyID, 2,
+        "Test Film #3 - Quote 2",
+        "");
+    const quote3 = quoteFactory(category, keyID, 3,
+        "Test Film #3 - Quote 3",
+        "");
+    const quote4 = quoteFactory(category, keyID, 4,
+        "Test Film #3 - Quote 4",
+        "");
+    const quote5 = quoteFactory(category, keyID, 5,
+        "Test Film #3 - Quote 5",
+        "");
+    return [quote1, quote2, quote3, quote4, quote5];
 }
 
 
